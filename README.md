dfc9a3afdb497dfff72bf45a97f60910f1c077ba
Daniel Taeyoung Sohn <sohn.d.50@gmail.com>
2023-04-25 21:07:00 -0400

    Moved folders up one level

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1><i>Turnwheel</i></h1>
+<h2><i>Turnwheel</i> is a tool to help users calculate builds for characters in the game Fire Emblem Engage!<h2>
+
+Deciding how to allocate resources can be hard, <i>Turnwheel</i> is here to help decide how you should spend them on what units.
+Users can build units and calculate their performance across chapters across the game to judge their performance across the game and to other units.
+<i>Turnwheel</i> will handle many of the calculations on its own end. Some examples are calculating growths, fixed and random, combat simulation against enemies in a chapter, and thresholds to reach breakpoints in performance.
---
+++ src/App.js
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import { supabase } from './supabaseClient';
+import { Container } from '@mui/material';
+import { BasicTabs } from './NavigationTabs';
+import './App.css';
+
+
+// Gets playable characters via Query to Supabase
+function Characters() 
+{
+  const [characterStats, setCharacters] = useState([]);
+  async function getCharacters() 
+  {
+    let { data: char_growth_rates, error } = await supabase
+    .from('char_growth_rates')
+    .select('*')
+    // Update the state
+    setCharacters(char_growth_rates);
+    if (error)
+    {
+      alert("Cannot get character data!")
+    }
+  }
+  // Execute the function
+  getCharacters();
+  // Below is what displays when you use <Library />
+  return (
+    <table>
+    {
+      <div>
+        <thead>
+          <tr>
+            <th scope = "col">Name</th>
+            <th scope = "col">Health</th>
+            <th scope = "col">Strength</th>
+            <th scope = "col">Magic</th>
+            <th scope = "col">Dexterity</th>
+            <th scope = "col">Speed</th>
+            <th scope = "col">Defense</th>
+            <th scope = "col">Resistance</th>
+            <th scope = "col">Luck</th>
+            <th scope = "col">Build</th>
+          </tr>
+        </thead>
+        {
+        characterStats.map((c)=> (
+          <tbody>
+            <th scope = "row">{c.Character}</th>
+            <td>{c.HP}</td>
+            <td>{c.Strength}</td>
+            <td>{c.Magic}</td>
+            <td>{c.Dexterity}</td>
+            <td>{c.Speed}</td>
+            <td>{c.Defense}</td>
+            <td>{c.Resistance}</td>
+            <td>{c.Luck}</td>
+            <td>{c.Build}</td>
+          </tbody>
+          ))
+        }
+      </div>
+    }
+    </table>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <BasicTabs />
+      </header>
+    </div>
+  );
+}
+
+export default App;
